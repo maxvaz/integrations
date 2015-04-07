46c486ec262832b5237c50e34811cd44a37396dd
mvazquez <mvazquez@dridco.com>
2015-04-07 16:30:12 -0300

    lpm!

---
+++ src/main/java/com/navent/integrations/igi/model/repository/PostMapping.java
@@ -0,0 +1,20 @@
+package com.navent.integrations.igi.model.repository;
+
+public class PostMapping {
+
+    private final Long providerPostId;
+    private final Long navPlatId;
+
+    public PostMapping(Long providerPostId, Long navPlatId) {
+        this.providerPostId = providerPostId;
+        this.navPlatId = navPlatId;
+    }
+
+    public Long getNavPlatId() {
+        return navPlatId;
+    }
+
+    public Long getProviderPostId() {
+        return providerPostId;
+    }
+}
